04279e43173704e5e17b62ca10d416a6bc8c1401
jamesrahhh <juntaotto777@gmail.com>
2023-12-25 22:08:43 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# neuralflight
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/neuralflight/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.neuralflight
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
