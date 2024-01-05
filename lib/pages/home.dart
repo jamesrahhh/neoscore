72e6f2e3015be0153556d245fef09d623345c8af
jamesrahhh <juntaotto777@gmail.com>
2024-01-05 15:40:28 -0600

    Fixed issues with custom font

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Center(child: Text("Home", style: TextStyle(color: Colors.white, fontFamily: "assets/fonts/Inter-Regular.ttf"))), backgroundColor: Colors.black,),
+      appBar: AppBar(title: const Center(child: Text("Home", style: TextStyle(color: Colors.white, fontFamily: "Inter"))), backgroundColor: Colors.black,),
       backgroundColor: Colors.grey,
     );
   }
