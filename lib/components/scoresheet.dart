2d6f012bb5cba5f453792256ee89c307eab70cde
jamesrahhh <juntaotto777@gmail.com>
2024-02-16 22:26:41 -0600

    Remove unnecessary class

--- lib/components/scoresheet.dart
+++ lib/components/scoresheet.dart
@@ -1,15 +1,9 @@
 class Scoresheet {
   final Target target;
   final int distance;
-  List<End> ends = [];
+  List<List<int>> ends = [];
 
   Scoresheet({required this.target, required this.distance});
-}
-
-class End {
-  final List<int> scores;
-
-  End({required this.scores});
 }
 
 enum Target { nfaa, usa }
