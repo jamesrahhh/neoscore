ebf9b1f34ffc10de390f34aa65407a0d45c221f4
jamesrahhh <juntaotto777@gmail.com>
2024-02-24 21:51:57 -0600

    Compact session.dart + add flutter_launcher_icons

--- lib/pages/major/session.dart
+++ lib/pages/major/session.dart
@@ -36,153 +36,125 @@ class _SessionState extends State<Session> {
       );
     } else {
       return Scaffold(
-        appBar: const CustomAppBar(pageName: 'Session'),
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            children: [
-              SessionInfoDisplay(currentSheetID: currentSheetID),
-              SessionScoresheetDisplay(currentSheetID: currentSheetID),
-              SessionBottomDisplay(currentSheetID: currentSheetID)
-            ],
-          ),
-        ), /*
-        floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.add_rounded),
-            onPressed: () {
-              setState(() {
-                _currentSheetID =
-                    ScoreHandler.createScoresheet(Target.nfaa, 20);
-              });
-              ScoreHandler.scoresheets[_currentSheetID].ends
-                  .add([5, 4, 4, 4, 3]);
-              ScoreHandler.scoresheets[_currentSheetID].ends
-                  .add([6, 6, 5, 5, 5]);
-            }),*/
-      );
-    }
-  }
-}
-
-class SessionInfoDisplay extends StatelessWidget {
-  final int currentSheetID;
-
-  const SessionInfoDisplay({super.key, required this.currentSheetID});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            'Scoresheet',
-            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-              '${ScoreHandler.scoresheets[currentSheetID].target.name.toUpperCase()} target, ${ScoreHandler.scoresheets[currentSheetID].distance} yards'),
-        )
-      ],
-    );
-  }
-}
-
-class SessionScoresheetDisplay extends StatelessWidget {
-  final int currentSheetID;
-
-  const SessionScoresheetDisplay({super.key, required this.currentSheetID});
-
-  @override
-  Widget build(BuildContext context) {
-    return Flexible(
-      child: ListView.builder(
-        itemCount: ScoreHandler.scoresheets[currentSheetID].ends.length,
-        itemBuilder: (BuildContext verticalContext, int endIndex) {
-          return SizedBox(
-            height: 40,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          appBar: const CustomAppBar(pageName: 'Session'),
+          backgroundColor: Colors.white,
+          body: Center(
+              child: Column(children: [
+            // Header display.
+            Row(
               children: [
-                SizedBox(
-                  width: 64,
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Text('End ${endIndex + 1}'),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Scoresheet',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
                   ),
                 ),
-                Flexible(
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: ScoreHandler
-                        .scoresheets[currentSheetID].ends[endIndex].length,
-                    itemBuilder:
-                        (BuildContext horizontalContext, int shotIndex) {
-                      double screenWidth = MediaQuery.of(context).size.width;
-                      return SizedBox(
-                        width: (screenWidth - 64) /
-                            (ScoreHandler.scoresheets[currentSheetID]
-                                .ends[endIndex].length),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                gradient: LinearGradient(colors: [
-                                  TargetHandler.getRingColors(
-                                      ScoreHandler
-                                          .scoresheets[currentSheetID].target,
-                                      ScoreHandler.scoresheets[currentSheetID]
-                                          .ends[endIndex][shotIndex])[0],
-                                  TargetHandler.getRingColors(
-                                      ScoreHandler
-                                          .scoresheets[currentSheetID].target,
-                                      ScoreHandler.scoresheets[currentSheetID]
-                                          .ends[endIndex][shotIndex])[1]
-                                ])),
-                            child: Center(
-                              child: Text(
-                                TargetHandler.parseScore(
-                                    ScoreHandler
-                                        .scoresheets[currentSheetID].target,
-                                    ScoreHandler.scoresheets[currentSheetID]
-                                        .ends[endIndex][shotIndex]),
-                                style: TextStyle(
-                                    color: TargetHandler.getRingColors(
-                                        ScoreHandler
-                                            .scoresheets[currentSheetID].target,
-                                        ScoreHandler.scoresheets[currentSheetID]
-                                            .ends[endIndex][shotIndex])[2]),
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                      '${ScoreHandler.scoresheets[currentSheetID].target.name.toUpperCase()} target, ${ScoreHandler.scoresheets[currentSheetID].distance} yards'),
+                )
               ],
             ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class SessionBottomDisplay extends StatelessWidget {
-  final int currentSheetID;
-
-  const SessionBottomDisplay({super.key, required this.currentSheetID});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Text(
-          'Score: ${ScoreHandler.getTotalScore(ScoreHandler.scoresheets[currentSheetID].target, currentSheetID)[0]}, X: ${ScoreHandler.getTotalScore(ScoreHandler.scoresheets[currentSheetID].target, currentSheetID)[1]}'),
-    );
+            // Score display.
+            Flexible(
+                child: ListView.builder(
+              itemCount: ScoreHandler.scoresheets[currentSheetID].ends.length,
+              itemBuilder: (BuildContext verticalContext, int endIndex) {
+                return SizedBox(
+                  height: 40,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        width: 64,
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Text('End ${endIndex + 1}'),
+                        ),
+                      ),
+                      Flexible(
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: ScoreHandler.scoresheets[currentSheetID]
+                              .ends[endIndex].length,
+                          itemBuilder:
+                              (BuildContext horizontalContext, int shotIndex) {
+                            double screenWidth =
+                                MediaQuery.of(context).size.width;
+                            return SizedBox(
+                              width: (screenWidth - 64) /
+                                  (ScoreHandler.scoresheets[currentSheetID]
+                                      .ends[endIndex].length),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      gradient: LinearGradient(colors: [
+                                        TargetHandler.getRingColors(
+                                            ScoreHandler
+                                                .scoresheets[currentSheetID]
+                                                .target,
+                                            ScoreHandler
+                                                .scoresheets[currentSheetID]
+                                                .ends[endIndex][shotIndex])[0],
+                                        TargetHandler.getRingColors(
+                                            ScoreHandler
+                                                .scoresheets[currentSheetID]
+                                                .target,
+                                            ScoreHandler
+                                                .scoresheets[currentSheetID]
+                                                .ends[endIndex][shotIndex])[1]
+                                      ])),
+                                  child: Center(
+                                    child: Text(
+                                      TargetHandler.parseScore(
+                                          ScoreHandler
+                                              .scoresheets[currentSheetID]
+                                              .target,
+                                          ScoreHandler
+                                              .scoresheets[currentSheetID]
+                                              .ends[endIndex][shotIndex]),
+                                      style: TextStyle(
+                                          color:
+                                              TargetHandler.getRingColors(
+                                                  ScoreHandler
+                                                      .scoresheets[
+                                                          currentSheetID]
+                                                      .target,
+                                                  ScoreHandler
+                                                          .scoresheets[
+                                                              currentSheetID]
+                                                          .ends[endIndex]
+                                                      [shotIndex])[2]),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                                'Score: ${ScoreHandler.getTotalScore(ScoreHandler.scoresheets[currentSheetID].target, currentSheetID)[0]}, X: ${ScoreHandler.getTotalScore(ScoreHandler.scoresheets[currentSheetID].target, currentSheetID)[1]}'),
+                          ),
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.save_rounded))
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+            ))
+          ])));
+    }
   }
 }
