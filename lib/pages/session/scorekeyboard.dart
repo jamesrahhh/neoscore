a67a15a08820d53f9b1b20e1fe28bd1efc010687
jamesrahhh <juntaotto777@gmail.com>
2024-04-12 10:37:00 -0500

    Fix ParentDataWidget exception (hopefully)

--- lib/pages/session/scorekeyboard.dart
+++ lib/pages/session/scorekeyboard.dart
@@ -67,95 +67,89 @@ class ScoreKeyboard extends StatelessWidget {
                     return Center(
                       child: SizedBox(
                         height: 60,
-                        child: Flexible(
-                          child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              shrinkWrap: true,
-                              itemCount: verticalIndex == 0
-                                  ? rowMembers.ceil()
-                                  : rowMembers.floor(),
-                              itemBuilder:
-                                  (horizontalContext, horizontalIndex) {
-                                return InkWell(
-                                  onTap: () {
-                                    if (ScoreHandler.scoresheets[currentSheetID]
-                                            .ends.length <=
-                                        endIndex) {
-                                      ScoreHandler
-                                          .scoresheets[currentSheetID].ends
-                                          .add([]);
-                                    }
-                                    if (ScoreHandler.scoresheets[currentSheetID]
-                                            .ends[endIndex].length <=
-                                        shotIndex) {
-                                      ScoreHandler.scoresheets[currentSheetID]
-                                          .ends[endIndex]
-                                          .add(0);
-                                    }
-                                    ScoreHandler.scoresheets[currentSheetID]
-                                            .ends[endIndex][shotIndex] =
-                                        horizontalIndex +
-                                            (verticalIndex == 0
-                                                ? 0
-                                                : rowMembers.ceil());
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            shrinkWrap: true,
+                            itemCount: verticalIndex == 0
+                                ? rowMembers.ceil()
+                                : rowMembers.floor(),
+                            itemBuilder: (horizontalContext, horizontalIndex) {
+                              return InkWell(
+                                onTap: () {
+                                  if (ScoreHandler.scoresheets[currentSheetID]
+                                          .ends.length <=
+                                      endIndex) {
+                                    ScoreHandler
+                                        .scoresheets[currentSheetID].ends
+                                        .add([]);
+                                  }
+                                  if (ScoreHandler.scoresheets[currentSheetID]
+                                          .ends[endIndex].length <=
+                                      shotIndex) {
                                     ScoreHandler.scoresheets[currentSheetID]
                                         .ends[endIndex]
-                                        .sort((b, a) => a.compareTo(b));
-                                    update();
-                                    Navigator.pop(context);
-                                  },
-                                  child: SizedBox(
-                                    width: (screenWidth - 30) /
-                                        ((verticalIndex == 0
-                                            ? rowMembers.ceil()
-                                            : rowMembers.floor())),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                            gradient: TargetHandler
-                                                .targets[ScoreHandler
-                                                    .scoresheets[currentSheetID]
-                                                    .targetIndex]
-                                                .getRingGradient(
-                                                    horizontalIndex +
-                                                        (verticalIndex == 0
-                                                            ? 0
-                                                            : rowMembers
-                                                                .ceil()))),
-                                        child: Center(
-                                            child: Text(
-                                                TargetHandler.parseScore(
-                                                    ScoreHandler
-                                                        .scoresheets[
-                                                            currentSheetID]
-                                                        .targetIndex,
-                                                    horizontalIndex +
-                                                        (verticalIndex == 0
-                                                            ? 0
-                                                            : rowMembers
-                                                                .ceil())),
-                                                style: TextStyle(
-                                                    color: TargetHandler
-                                                        .targets[ScoreHandler
-                                                            .scoresheets[
-                                                                currentSheetID]
-                                                            .targetIndex]
-                                                        .getTextColor(
-                                                            horizontalIndex +
-                                                                (verticalIndex ==
-                                                                        0
-                                                                    ? 0
-                                                                    : rowMembers
-                                                                        .ceil()))))),
-                                      ),
+                                        .add(0);
+                                  }
+                                  ScoreHandler.scoresheets[currentSheetID]
+                                          .ends[endIndex][shotIndex] =
+                                      horizontalIndex +
+                                          (verticalIndex == 0
+                                              ? 0
+                                              : rowMembers.ceil());
+                                  ScoreHandler.scoresheets[currentSheetID]
+                                      .ends[endIndex]
+                                      .sort((b, a) => a.compareTo(b));
+                                  update();
+                                  Navigator.pop(context);
+                                },
+                                child: SizedBox(
+                                  width: (screenWidth - 30) /
+                                      ((verticalIndex == 0
+                                          ? rowMembers.ceil()
+                                          : rowMembers.floor())),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          gradient: TargetHandler
+                                              .targets[ScoreHandler
+                                                  .scoresheets[currentSheetID]
+                                                  .targetIndex]
+                                              .getRingGradient(horizontalIndex +
+                                                  (verticalIndex == 0
+                                                      ? 0
+                                                      : rowMembers.ceil()))),
+                                      child: Center(
+                                          child: Text(
+                                              TargetHandler.parseScore(
+                                                  ScoreHandler
+                                                      .scoresheets[
+                                                          currentSheetID]
+                                                      .targetIndex,
+                                                  horizontalIndex +
+                                                      (verticalIndex == 0
+                                                          ? 0
+                                                          : rowMembers.ceil())),
+                                              style: TextStyle(
+                                                  color: TargetHandler
+                                                      .targets[ScoreHandler
+                                                          .scoresheets[
+                                                              currentSheetID]
+                                                          .targetIndex]
+                                                      .getTextColor(
+                                                          horizontalIndex +
+                                                              (verticalIndex ==
+                                                                      0
+                                                                  ? 0
+                                                                  : rowMembers
+                                                                      .ceil()))))),
                                     ),
                                   ),
-                                );
-                              }),
-                        ),
+                                ),
+                              );
+                            }),
                       ),
                     );
                   }),
