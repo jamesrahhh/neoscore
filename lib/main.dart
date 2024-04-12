d21465bb578f5d108b3e934bb6bd303fd8922759
jamesrahhh <juntaotto777@gmail.com>
2024-04-12 10:46:43 -0500

    Move pages to individual folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:neuralflight/components/navigation/navigationmodel.dart';
-import 'package:neuralflight/pages/account.dart';
+import 'package:neuralflight/pages/account/account.dart';
 import 'package:neuralflight/pages/session/sessionactive.dart';
 import 'package:neuralflight/pages/session/sessionpassive.dart';
-import 'package:neuralflight/pages/history.dart';
-import 'package:neuralflight/pages/insights.dart';
-import 'package:neuralflight/pages/settings.dart';
+import 'package:neuralflight/pages/history/history.dart';
+import 'package:neuralflight/pages/insights/insights.dart';
+import 'package:neuralflight/pages/settings/settings.dart';
 import 'package:provider/provider.dart';
 
 void main() {
