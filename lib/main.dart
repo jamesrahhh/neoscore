08012bcc872d7c88ece7d0d656e2e1627bd2043a
jamesrahhh <juntaotto777@gmail.com>
2024-10-09 13:54:15 -0500

    main: Add basic FlexColorScheme theme.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,13 +8,74 @@ void main() {
 class Neoscore extends StatelessWidget {
   const Neoscore({super.key});
 
+  final TextTheme _textTheme = const TextTheme(
+    displaySmall:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 800)]),
+    displayMedium:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 800)]),
+    displayLarge:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 800)]),
+    headlineSmall:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 700)]),
+    headlineMedium:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 700)]),
+    headlineLarge:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 700)]),
+    titleSmall:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 600)]),
+    titleMedium:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 600)]),
+    titleLarge:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 600)]),
+    bodySmall:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 400)]),
+    bodyMedium:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 400)]),
+    bodyLarge:
+        TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 400)]),
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Neoscore',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
+      theme: FlexThemeData.light(
+          textTheme: _textTheme.apply(
+            bodyColor: Colors.black,
+            displayColor: Colors.black,
+          ),
+          colors: const FlexSchemeColor(
+            primary: Colors.black,
+            primaryContainer: Colors.black,
+            secondary: Colors.black,
+            secondaryContainer: Colors.black,
+            tertiary: Colors.black,
+            tertiaryContainer: Colors.white,
+            error: Colors.red,
+            errorContainer: Colors.white,
+          ),
+          fontFamily: 'Montserrat',
+          useMaterial3: true,
+          visualDensity: FlexColorScheme.comfortablePlatformDensity),
+      darkTheme: FlexThemeData.dark(
+          textTheme: _textTheme.apply(
+            bodyColor: Colors.white,
+            displayColor: Colors.white,
+          ),
+          colors: const FlexSchemeColor(
+            primary: Colors.white,
+            primaryContainer: Colors.white,
+            secondary: Colors.white,
+            secondaryContainer: Colors.white,
+            tertiary: Colors.white,
+            tertiaryContainer: Colors.white,
+            error: Colors.red,
+            errorContainer: Colors.black,
+          ),
+          fontFamily: 'Montserrat',
+          useMaterial3: true,
+          visualDensity: FlexColorScheme.comfortablePlatformDensity),
+      themeMode: ThemeMode.system,
       home: const Home(),
     );
   }
@@ -24,9 +86,12 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text('Neoscore',
-            style: TextStyle(
-                decoration: TextDecoration.none, color: Colors.white)));
+    return Scaffold(
+        body: Center(
+      child: Text(
+        'Neoscore',
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+    ));
   }
 }
