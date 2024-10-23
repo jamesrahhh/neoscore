718d77f99e306ec96a6857fa4a37b567e2ef306d
jamesrahhh <juntaotto777@gmail.com>
2024-10-23 15:10:01 -0500

    scoresheet: Start basic layout.

--- lib/ui/views/scoresheet/scoresheet_view.dart
+++ lib/ui/views/scoresheet/scoresheet_view.dart
@@ -6,5 +6,19 @@ class ScoresheetView extends StatelessWidget {
   const ScoresheetView({super.key});
 
   @override
-  Widget build(BuildContext context) => const ScoresheetCard();
+  Widget build(BuildContext context) => Column(children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0.0),
+          child: Row(
+            children: <Widget>[
+              Text(
+                'Generic Header',
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+            ],
+          ),
+        ),
+        const ScoresheetCard(),
+        const ScoresheetCard()
+      ]);
 }
--- lib/ui/widgets/scoresheet_view/scoresheet_card.dart
+++ lib/ui/widgets/scoresheet_view/scoresheet_card.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 
-import '../shared/circle_icon.dart';
-
 class ScoresheetCard extends StatelessWidget {
   const ScoresheetCard({super.key});
 
   @override
-  Widget build(BuildContext context) => Center(
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          const Padding(
-              padding: EdgeInsets.all(6.0), child: CircleIcon(radius: 35)),
-          Padding(
-            padding: const EdgeInsets.all(6.0),
-            child: Text(
-              'Scoresheet',
-              style: Theme.of(context).textTheme.titleMedium,
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0.0),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Scoresheet Name',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    const Icon(Icons.more_vert),
+                  ],
+                ),
+                Divider(
+                  color: Theme.of(context).colorScheme.secondary,
+                )
+              ],
             ),
           ),
-        ]),
+        ),
       );
 }
