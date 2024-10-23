b658d350dfde11fbad7b322dcf652cfc13b6acdc
jamesrahhh <juntaotto777@gmail.com>
2024-10-23 15:10:12 -0500

    theme: Minor tweaks.

--- lib/ui/theme/theme.dart
+++ lib/ui/theme/theme.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 const TextTheme _baseTextTheme = TextTheme(
   displaySmall:
       TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 800)]),
-  displayMedium:
-      TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 800)]),
+  displayMedium: TextStyle(
+      fontSize: 28,
+      fontVariations: <FontVariation>[FontVariation('wght', 800)]),
   displayLarge:
       TextStyle(fontVariations: <FontVariation>[FontVariation('wght', 800)]),
   titleSmall:
@@ -28,12 +29,16 @@ final ThemeData _baseTheme = ThemeData(
 
 final ThemeData lightTheme = _baseTheme.copyWith(
   brightness: Brightness.light,
+  iconTheme: const IconThemeData(color: Color(0xFF969696)),
+  textTheme: _baseTextTheme.apply(
+    displayColor: const Color(0xFF1C1C1C),
+    bodyColor: const Color(0xFF575757),
+  ),
   colorScheme: const ColorScheme(
     brightness: Brightness.light,
     error: Color(0xFFF84141),
     onError: Color(0xFF575757),
     onPrimary: Color(0xFF575757),
-    onPrimaryContainer: Color(0xFF575757),
     onSecondary: Color(0xFF575757),
     onSurface: Color(0xFF575757),
     outline: Color(0xFFE8E8E8),
@@ -44,23 +49,21 @@ final ThemeData lightTheme = _baseTheme.copyWith(
     surface: Color(0xFFF7F7F7),
     surfaceContainer: Colors.white,
     surfaceContainerLow: Colors.white,
-    tertiary: Color(0xFF969696),
-  ),
-  scaffoldBackgroundColor: const Color(0xFFF7F7F7),
-  textTheme: _baseTextTheme.apply(
-    displayColor: const Color(0xFF1C1C1C),
-    bodyColor: const Color(0xFF575757),
   ),
 );
 
 final ThemeData darkTheme = _baseTheme.copyWith(
   brightness: Brightness.dark,
+  iconTheme: const IconThemeData(color: Color(0xFF707070)),
+  textTheme: _baseTextTheme.apply(
+    displayColor: const Color(0xFFE8E8E8),
+    bodyColor: const Color(0xFFB8B8B8),
+  ),
   colorScheme: const ColorScheme(
     brightness: Brightness.dark,
     error: Color(0xFFE92C2C),
     onError: Color(0xFFB8B8B8),
     onPrimary: Color(0xFFB8B8B8),
-    onPrimaryContainer: Color(0xFFB8B8B8),
     onSecondary: Color(0xFFB8B8B8),
     onSurface: Color(0xFFB8B8B8),
     outline: Color(0xFF292929),
@@ -72,9 +75,5 @@ final ThemeData darkTheme = _baseTheme.copyWith(
     surfaceContainer: Color(0xFF111111),
     surfaceContainerLow: Color(0xFF111111),
     tertiary: Color(0xFF707070),
-  ),
-  textTheme: _baseTextTheme.apply(
-    displayColor: const Color(0xFFE8E8E8),
-    bodyColor: const Color(0xFFB8B8B8),
   ),
 );
