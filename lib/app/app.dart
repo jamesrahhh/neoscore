c0e03f3ff96aed1c33acac9dad36bff45cb73aa9
jamesrahhh <juntaotto777@gmail.com>
2024-11-02 20:39:50 -0500

    meta: Move app widget to seperate file.

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../util/theme/theme.dart';
+import '../view/navigation/navigation_view.dart';
+
+class Neoscore extends StatelessWidget {
+  const Neoscore({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Neoscore',
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        home: const NavigationView(),
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,8 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'ui/theme/theme.dart';
-import 'ui/views/navigation/navigation_view.dart';
-import 'utils/firebase/firebase_options.dart';
+import 'app/app.dart';
+import 'util/firebase/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,16 +21,4 @@ Future<void> main() async {
   };
 
   runApp(const Neoscore());
-}
-
-class Neoscore extends StatelessWidget {
-  const Neoscore({super.key});
-
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Neoscore',
-        theme: lightTheme,
-        darkTheme: darkTheme,
-        home: const NavigationView(),
-      );
 }
