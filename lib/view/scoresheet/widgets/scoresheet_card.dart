4a27b6ea81eb9953781349a7cadc9852caa7b48d
jamesrahhh <juntaotto777@gmail.com>
2025-02-24 23:21:20 +0000

    feat: Remove scoresheet card rounding

--- lib/view/scoresheet/widgets/scoresheet_card.dart
+++ lib/view/scoresheet/widgets/scoresheet_card.dart
@@ -11,53 +11,50 @@ class ScoresheetCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
-        child: Container(
-          decoration: BoxDecoration(
-            border:
-                Border.all(color: Theme.of(context).colorScheme.onSecondary),
-            borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
+    padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Theme.of(context).colorScheme.onSecondary),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Scoresheet ${index + 1}',
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    const Icon(Icons.more_vert),
-                  ],
+                Text(
+                  'Scoresheet ${index + 1}',
+                  style: Theme.of(context).textTheme.titleMedium,
                 ),
-                Divider(
-                  color: Theme.of(context).colorScheme.onSecondary,
-                ),
-                Column(
+                const Icon(Icons.more_vert),
+              ],
+            ),
+            Divider(color: Theme.of(context).colorScheme.onSecondary),
+            Column(
+              children: List<Widget>.generate(
+                2,
+                (int endIndex) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: List<Widget>.generate(
-                    2,
-                    (int endIndex) => Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: List<Widget>.generate(
-                        Provider.of<ScoresheetViewModel>(context)
-                            .getScoresheet(index)
-                            .shotsPerEnd,
-                        (int shotIndex) => ScoreIcon(
-                          value: Provider.of<ScoresheetViewModel>(context)
+                    Provider.of<ScoresheetViewModel>(
+                      context,
+                    ).getScoresheet(index).shotsPerEnd,
+                    (int shotIndex) => ScoreIcon(
+                      value:
+                          Provider.of<ScoresheetViewModel>(context)
                               .getScoresheet(index)
                               .scoreData[endIndex][shotIndex]
                               .toString(),
-                          color: Colors.blueAccent,
-                        ),
-                      ),
+                      color: Colors.blueAccent,
                     ),
                   ),
-                )
-              ],
+                ),
+              ),
             ),
-          ),
+          ],
         ),
-      );
+      ),
+    ),
+  );
 }
