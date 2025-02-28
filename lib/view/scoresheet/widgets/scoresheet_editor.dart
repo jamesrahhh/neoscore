320b010125ced1c9f8558b6ed8f2ad1432f1c004
jamesrahhh <juntaotto777@gmail.com>
2025-02-28 00:25:03 +0000

    feat: Basic scoresheet display

--- lib/view/scoresheet/widgets/scoresheet_editor.dart
+++ lib/view/scoresheet/widgets/scoresheet_editor.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../scoresheet_viewmodel.dart';
+import 'score_icon.dart';
 
 class ScoresheetEditor extends StatelessWidget {
-  const ScoresheetEditor({super.key});
+  const ScoresheetEditor({super.key, required this.index});
+
+  final int index;
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -18,6 +21,37 @@ class ScoresheetEditor extends StatelessWidget {
               listen: false,
             ).setPage(0),
         icon: const Icon(Icons.arrow_left),
+      ),
+    ),
+    body: Column(
+      children: List<Widget>.generate(
+        2,
+        (int endIndex) => Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children:
+              <Widget>[Text('${endIndex + 1}')] +
+              List<Widget>.generate(
+                Provider.of<ScoresheetViewModel>(
+                  context,
+                ).getScoresheet(index).shotsPerEnd,
+                (int shotIndex) => ScoreIcon(
+                  value:
+                      Provider.of<ScoresheetViewModel>(context)
+                          .getScoresheet(index)
+                          .scoreData[endIndex][shotIndex]
+                          .toString(),
+                  color: Colors.blueAccent,
+                ),
+              ) +
+              <Widget>[
+                Text(
+                  '${Provider.of<ScoresheetViewModel>(context).getScoresheet(index).getSingleScoreEnd(endIndex, Provider.of<ScoresheetViewModel>(context).getScoresheet(index).highestScore + 1)}',
+                ),
+                Text(
+                  '${Provider.of<ScoresheetViewModel>(context).getScoresheet(index).getTotalScoreEnd(endIndex)}',
+                ),
+              ],
+        ),
       ),
     ),
   );
