c4ee03f2b494ddd6612cb9af378c9c86a3f7a39c
jamesrahhh <juntaotto777@gmail.com>
2025-02-28 00:22:26 +0000

    chore: Add more helper functions to scoresheet object

--- lib/common/scoresheet/scoresheet.dart
+++ lib/common/scoresheet/scoresheet.dart
@@ -9,6 +9,24 @@ class Scoresheet {
   int shotsPerEnd;
   int highestScore;
 
+  int getTotalScoreEnd(int end) {
+    int total = 0;
+    for (final int shot in scoreData[end]) {
+      total += shot;
+    }
+    return total;
+  }
+
+  int getSingleScoreEnd(int end, int score) {
+    int total = 0;
+    for (final int shot in scoreData[end]) {
+      if (shot == score) {
+        total++;
+      }
+    }
+    return total;
+  }
+
   int get getTotalScore {
     int total = 0;
     for (final List<int> end in scoreData) {
